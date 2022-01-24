9e438dfdc7657f03e7d337a9242e41bbaced59c6
Ege Okhan <95880067+egeokhan@users.noreply.github.com>
2022-01-24 10:05:48 +0300

    Developed...

--- bankaccount-app/bankaccount_app.py
+++ bankaccount-app/bankaccount_app.py
@@ -1,3 +1,6 @@
+print("----------------------------------------------")
+print("Welcome to the Bank:")
+print("----------------------------------------------")
 class Account:
     def __init__(self,name,number,balance,password):
         self.name = name
@@ -24,8 +27,10 @@ class Account:
         self.list.append(filenew.readlines())
         filenew.close()
         if(login == False):
+            print("----------------------------------------------")
             print("We couldn't find your account...")
             question = input("Would you like to create an account? Y/N: ")
+            print("----------------------------------------------")
             result = False
             if question.upper() == "Y":
                 file1 = open("Accounts.txt","a")
@@ -41,37 +46,72 @@ class Account:
                     createnumber = input("Number: ")
                     file1.write("\n{};{};{};0".format(createusername,createpassword,createnumber))
                     file1.close()
+                    print("----------------------------------------------")
                     print("Your account has been created. You can login.")
+                    print("----------------------------------------------")
                 elif result == True:
+                    print("----------------------------------------------")
                     print("Please enter a different username...")
+                    print("----------------------------------------------")
             elif question.upper() == "N":
                 print("Okay, Goodbye!")
+                print("----------------------------------------------")
         elif(login == True):
+            print("----------------------------------------------")
             print("Login successful...")
+            account.Menu()
+    def Menu(self):
+        print("----------------------------------------------")
+        select = int(input("Select an Action:\nPress 1 to account information\nPress 2 to withdraw money\nPress 3 to add money\nPress 4 to change your information\nPress 5 to Exit\nPress: "))
+        if select == 1:
             account.AccountInfo()
-            select = int(input("Select an Action:\nPress 1 for account information\nPress 2 to withdraw money\nPress 3 to add money\nPress 4 to change your information\nPress: "))
-            if select == 1:
-                account.AccountInfo()
-            elif select == 2:
-                amount = int(input("Amount to be withdrawn: "))
-                account.WithdrawMoney(amount)
-            elif select == 3:
-                amount = int(input("Amount to add: "))
-                account.AddMoney(amount)
-            elif select == 4:
-                newname = input("New Name: ")
-                newpassword = input("New Password: ")
-                newnumber = input("New Number: ")
-                account.ChangeInformation(newname,newpassword,newnumber)
+        elif select == 2:
+            print("----------------------------------------------")
+            amount = int(input("Amount to be withdrawn: "))
+            account.WithdrawMoney(amount)
+        elif select == 3:
+            print("----------------------------------------------")
+            amount = int(input("Amount to add: "))
+            account.AddMoney(amount)
+        elif select == 4:
+            print("----------------------------------------------")
+            newname = input("New Name: ")
+            newpassword = input("New Password: ")
+            newnumber = input("New Number: ")
+            account.ChangeInformation(newname,newpassword,newnumber)
+        elif select == 5:
+            print("----------------------------------------------")
+            print("Goodbye!")
+            print("----------------------------------------------")
+            exit()
+
 ############################################################################################################
     def AccountInfo(self):
+        print("----------------------------------------------")
         print("Name:",self.name)
         print("Number:",self.number)
         print("Balance:",self.balance)
+        print("----------------------------------------------")
+        backmenu = input("Press 1 to return to the menu\nPress 2 to exit\nPress: ")
+        if backmenu == "1":
+            account.Menu()
+        else:
+            print("----------------------------------------------")
+            print("Goodbye!")
+            print("----------------------------------------------")
+            exit()
 ############################################################################################################
     def WithdrawMoney(self,amount):
         if(int(self.balance) - amount < 0):
             print("You do not have enough balance.")
+            backmenu = input("Press 1 to Go to the menu\nPress 2 to exit\nPress: ")
+            if backmenu == "1":
+                account.Menu()
+            else:
+                print("----------------------------------------------")
+                print("Goodbye!")
+                print("----------------------------------------------")
+                exit()
         else:
             oldbalance = self.balance
             self.balance -= amount
@@ -99,6 +139,15 @@ class Account:
                     updatefile.writelines("{}\n".format(self.list[num]))
                 num += 1
             updatefile.close()
+            print("----------------------------------------------")
+            backmenu = input("Press 1 to Go to the menu\nPress 2 to exit\nPress: ")
+            if backmenu == "1":
+                account.Menu()
+            else:
+                print("----------------------------------------------")
+                print("Goodbye!")
+                print("----------------------------------------------")
+                exit()
 ############################################################################################################
     def AddMoney(self,amount):
         oldbalance = self.balance
@@ -127,6 +176,15 @@ class Account:
                 updatefile.writelines("{}\n".format(self.list[num]))
             num += 1
         updatefile.close()
+        print("----------------------------------------------")
+        backmenu = input("Press 1 to return to the menu\nPress 2 to exit\nPress: ")
+        if backmenu == "1":
+            account.Menu()
+        elif backmenu == "2":
+            print("----------------------------------------------")
+            print("Goodbye!")
+            print("----------------------------------------------")
+            exit()
 ############################################################################################################        
     def ChangeInformation(self,newname,newpassword,newnumber):
         updatefile2 = open("Accounts.txt","r")
@@ -139,16 +197,17 @@ class Account:
                 break
         updatefile2.close()
         if result == False:
-            verification = input("Your information will be changed, are you sure? Y/N: ")
-            if(verification.lower() == "y"):
+            verification = input("Your information will be changed, are you sure? Yes/No: ")
+            if(verification.lower() == "yes"):
                 self.name = newname
                 self.password = newpassword
                 self.number = newnumber
                 print("Your Information Has Been Changed:")
                 account.AccountInfo()
-            elif(verification.lower() == "n"):
+            elif(verification.lower() == "no"):
+                print("----------------------------------------------")
                 print("Transaction canceled:")
-                account.AccountInfo()     
+                account.AccountInfo() 
             updatefile3 = open("Accounts.txt","r")
             meter1 = 0
             self.list.clear()
@@ -172,7 +231,17 @@ class Account:
                 num += 1
             updatefile.close()
         elif result == True:
+            print("----------------------------------------------")
             print("Please enter a different username...")
+            print("----------------------------------------------")
+        backmenu = input("Press 1 to return to the menu\nPress 2 to exit\nPress: ")
+        if backmenu == "1":
+            account.Menu()
+        else:
+            print("----------------------------------------------")
+            print("Goodbye!")
+            print("----------------------------------------------")
+            exit()
 ############################################################################################################
 name = input("Username: ")    
 password = input("Password: ")
